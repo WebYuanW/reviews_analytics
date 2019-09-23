54685cad2344c1a8950c5763f7c112926662ebea
Web Yuan <web71212181@gmail.com>
2019-09-23 22:49:59 +0800

    Add keyword searching function

--- ra.py
+++ ra.py
@@ -19,3 +19,18 @@ for d in data:
     if len(d) < 100:
         new.append(d)
 print('共有', len(new), '筆留言長度小於100')
+
+# 關鍵字搜尋
+q = input('是否進行關鍵字搜尋？')
+if q == 'yes':
+    while True:
+        x = input('請輸入欲查詢關鍵字')
+        key = []
+        for d in data:
+            if x in d:
+                key.append(d)
+        print('總共有', len(key), '筆資料符合關鍵字', x)
+        rq = input('是否換下個關鍵字？')
+        if rq == 'no':
+            break
+        
